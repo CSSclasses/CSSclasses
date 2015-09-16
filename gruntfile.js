5f08ca22c16d2b3bb10c448591779ef9085ad95f
verpixelt <kevin.lorenz@hitfoxgroup.com>
2015-09-16 16:55:53 +0200

    Copy files

---
+++ gruntfile.js
@@ -0,0 +1,113 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    require('time-grunt')(grunt); // Show elapsed time after tasks run to visualize performance
+    require('load-grunt-tasks')(grunt); // Load all Grunt tasks that are listed in package.json automagically
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllServe: { command: 'jekyll serve' },
+            jekyllBuild: { command: 'jekyll build' }
+
+        },
+
+        // watch for files to change and run tasks when they do
+        watch: {
+            sass: {
+                files: ['_scss/**/*.{scss,sass}'],
+                tasks: ['sass'],
+                options: { livereload: true } //using the browserextension
+            }
+        },
+
+        // sass (libsass) config
+        sass: {
+            options: {
+                sourceMap: true,
+                relativeAssets: false,
+                outputStyle: 'expanded',
+                sassDir: '_sass',
+                cssDir: '_site/css'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '_scss/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: '_site/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'sass',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: { logConcurrentOutput: true }
+        },
+
+        postcss: {
+          options: {
+            map: false,
+            processors: [
+              require('autoprefixer')({browsers: 'last 2 versions'}),
+              require('cssnano')()
+            ]
+          },
+          dist: {
+            src: '_site/css/*.css'
+          }
+        },
+
+        copy: {
+          vendor: {
+            files: [{
+              expand: true,
+              cwd: "bower_components/classie/",
+              src: "classie.js",
+              dest: "vendor/js"
+            },
+            {
+              expand: true,
+              cwd: "bower_components/reqwest/",
+              src: "reqwest.min.js",
+              dest: "vendor/js"
+            },
+            {
+              expand: true,
+              cwd: "bower_components/snap.svg/dist/",
+              src: "snap.svg-min.js",
+              dest: "vendor/js"
+            },
+            {
+              expand: true,
+              cwd: "bower_components/strftime/",
+              src: "strftime-min.js",
+              dest: "vendor/js"
+            }]
+          }
+        }
+    });
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', [ 'concurrent:serve' ]);
+
+    // Register the grunt build task
+    grunt.registerTask('build', [
+        'shell:jekyllBuild',
+        'sass',
+        'postcss',
+        'copy:vendor'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'serve');
+};
