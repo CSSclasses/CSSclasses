01a2556f1380f5b53c38492f6ee502c6f44f8804
verpixelt <kevin.lorenz@hitfoxgroup.com>
2015-09-16 18:15:58 +0200

    Dunno what happened here

--- gruntfile.js
+++ gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function (grunt) {
                     expand: true,
                     cwd: '_sass/',
                     src: ['**/*.{scss,sass}'],
-                    dest: '_site/css',
+                    dest: '_site/css/',
                     ext: '.css'
                 }]
             }
